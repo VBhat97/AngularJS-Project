fe469d69ac64d834653f33c477ed37b3e177f401
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-07-22 00:55:17 +0530

    Corrected Bootstrap and added routes

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider){
+
+    Stamplay.init("blogit");
+
+    $routeProvider
+        .when('/',{
+            templateUrl : 'templates/home.html'
+        })
+
+        .when('/login',{
+            templateUrl : 'templates/login.html'
+        })
+})
 
 app.controller('myCtrl', function($scope){
 
