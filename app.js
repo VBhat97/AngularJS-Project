b171c891b1701f578098dd7a60e874e76551ac55
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-07-26 01:12:25 +0530

    signup page added

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 var app = angular.module('myApp', ["ngRoute"]);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.hashPrefix('');
 
     Stamplay.init("blogit");
 
     $routeProvider
         .when('/',{
-            templateUrl : 'templates/home.html'
+            templateUrl : 'templates/home.html',
+            controller : "HomeCtrl"
         })
 
         .when('/login',{
-            templateUrl : 'templates/login.html'
+            templateUrl : 'templates/login.html',
+            controller : "LoginCtrl"
+        })
+
+        .when('/signup',{
+            templateUrl : 'templates/signup.html',
+            controller : "SignupCtrl"
         })
 })
 
-app.controller('myCtrl', function($scope){
+app.controller('SignupCtrl',function(){
 
-    $scope.firstname = "Vaibhav";
-    $scope.lastname = "Bhat";
+
+});
+
+app.controller('HomeCtrl',function(){
+
+
+});
+
+app.controller('LoginCtrl',function(){
+
+
+});
+
+app.controller('myCtrl',function($scope){
+
+
 });
