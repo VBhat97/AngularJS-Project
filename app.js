24df0640118b08f9112c22bf35c13bac9feeda20
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-07-28 01:06:25 +0530

    Added login and signup .htmls

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ app.config(function($routeProvider, $locationProvider){
 
     $locationProvider.hashPrefix('');
 
-    Stamplay.init("blogit");
+    Stamplay.init("blogitangularjs");
+
+    localStorage.removeItem('127.0.0.1-jwt');
 
     $routeProvider
         .when('/',{
@@ -23,9 +25,33 @@ app.config(function($routeProvider, $locationProvider){
         })
 })
 
-app.controller('SignupCtrl',function(){
+app.controller('SignupCtrl',function($scope){
 
-
+    $scope.newUser = {};
+    $scope.signup = function(){
+        if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmpassword)
+            {
+                console.log("All fields are valid");
+                if ($scope.newUser.password == $scope.newUser.confirmpassword)
+                    {
+                        console.log("All Good lets sign up!");
+                        Stamplay.User.signup($scope.newUser)
+                        .then(function(response){
+                            console.log(response);
+                        },function(error){
+                            console.log(error);
+                        });
+                    }
+                else
+                    {
+                        console.log("Passwords do not match");
+                    }
+            }
+        else
+            {
+                console.log("Some fields are invalid");
+            }
+    }
 });
 
 app.controller('HomeCtrl',function(){
@@ -33,9 +59,26 @@ app.controller('HomeCtrl',function(){
 
 });
 
-app.controller('LoginCtrl',function(){
+app.controller('LoginCtrl',function($scope){
 
+    $scope.login = function(){
 
+        Stamplay.User.currentUser()
+        .then(function(res){
+            console.log("Logged in " + res);
+            if(res.user){
+              $timeout(function(){
+                $location.path( "/viewBlogs" )
+                });
+            }
+            else{
+                //proceed with login
+            }
+        },
+        function(error){
+            console.log(error);
+        });
+    }
 });
 
 app.controller('myCtrl',function($scope){
