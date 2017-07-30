4e8796ae5e3e25f41e4adf3f7cab429711354436
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-07-30 18:54:53 +0530

    added further ui-router

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 var app = angular.module('myApp', ["ui.router"]);
 
 app.run(function($rootScope){
-
-    Stamplay.User.connectUser()
-        .then(function(res){
-            if(res.user)
-                {
-                    $rootScope.loggedIn = true;
-                    console.log($rootScope.loggedIn);
-                }
-            else
-                {
-                    $rootScope.loggedIn = false;
-                    console.log($rootScope.loggedIn);
-                }
-        }, function(err)
+    Stamplay.User.currentUser()
+    .then(function(res){
+        if(res.user)
             {
-                console.log("An error occured while getting current user!");
-            });
-})
+                $rootScope.loggedIn = true;
+                console.log($rootScope.loggedIn);
+            }
+        else
+            {
+                $rootScope.loggedIn = false;
+                console.log($rootScope.loggedIn);
+            }
+    }, function(err){
+        console.log("An error occured while getting the user");
+    });
 
-app.config(function($stateProvider, $urlRouterProvider){
+});
+
+app.config(function($routeProvider, $urlRouterProvider){
 
     $locationProvider.hashPrefix('');
 
@@ -30,30 +29,107 @@ app.config(function($stateProvider, $urlRouterProvider){
 
     $stateProvider
         .state('home', {
-            url: '/',
-            templateUrl: 'templates/home.html',
-            controller: 'HomeCtrl'
+            url : "/home",
+            templateUrl : "templates/home.html",
+            controller : 'HomeCtrl'
         })
+
         .state('login', {
-            url : '/login',
-            templateUrl : 'templates/login.html',
+            url : "/home",
+            templateUrl : "templates/home.html",
             controller : 'LoginCtrl'
-        })
+        } )
+
         .state('signup', {
-            url : '/signup',
-            templateUrl : 'templates/signup.html',
+            url : "/signup",
+            templateUrl : "templates/signup.html",
             controller : 'SignupCtrl'
         })
-        .state('myblogs' , {
-            url : '/myblogs',
-            templateUrl : '/myblogs.html',
-            controller : 'MyBlogCtrl'
-        })
 
-        $urlRouterProvider.otherwise("/");
+        .state('myblogs', {
+            url : "/myblogs",
+            templateUrl : "templates/myblogs.html",
+            controller : 'MyblogsCtrl'
+        });
+
+        urlRouterProvider.otherwise("/");
+})
+
+app.controller('MyblogsCtrl', function($scope){
+
 });
 
-app.controller('MyBlogCtrl',function($scope){
+app.controller('SignupCtrl',function($scope){
+
+    $scope.newUser = {};
+    $scope.signup = function(){
+        if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmpassword)
+            {
+                console.log("All fields are valid");
+                if ($scope.newUser.password == $scope.newUser.confirmpassword)
+                    {
+                        console.log("All Good lets sign up!");
+                        Stamplay.User.signup($scope.newUser)
+                        .then(function(response){
+                            console.log(response);
+                        },function(error){
+                            console.log(error);
+                        });
+                    }
+                else
+                    {
+                        console.log("Passwords do not match");
+                    }
+            }
+        else
+            {
+                console.log("Some fields are invalid");
+            }
+    }
+});
+
+app.controller('HomeCtrl',function(){
 
 
-})
+});
+
+app.controller('LoginCtrl',function($scope,$state,$rootScope,$timeout){
+
+    $scope.login = function(){
+
+        Stamplay.User.currentUser()
+        .then(function(res){
+            console.log("Logged in " + res);
+            if(res.user){
+              $rootScope.loggedIn = true ;
+              $rootScope.displayName = res.user.firstName + "." + res.user.lastName ;
+              $timeout(function(){
+                $state.go( "/viewBlogs" )
+                });
+            }
+            else{
+                //proceed with login
+                Stamplay.User.login($scope.user)
+                .then(function(res){
+                    console.log(res);
+                    $rootScope.loggedIn = true;
+                    $rootScope.displayName = res.user.firstName+ "." + res.user.lastName;
+                    $timeout(function(){
+                        $state.go("/viewblogs");
+                    });
+                }, function(err){
+                    console.log(err);
+                    $rootScope.loggedIn = false;
+                })
+            }
+        },
+        function(error){
+            console.log(error);
+        });
+    }
+});
+
+app.controller('myCtrl',function($scope){
+
+
+});
